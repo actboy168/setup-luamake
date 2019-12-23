c4f486784a46379776eccd81f9b3935aed93654a
actboy168 <actboy168@gmail.com>
2019-12-23 15:29:33 +0800

    自己安装msvc

--- dist/index.js
+++ dist/index.js
@@ -94,6 +94,18 @@ const core = __webpack_require__(626)
 const exec = __webpack_require__(28)
 const process = __webpack_require__(765)
 const path = __webpack_require__(622)
+const execFile = __webpack_require__(669).promisify(__webpack_require__(129).execFile)
+
+const InterestingVariables = [
+    'INCLUDE',
+    'LIB',
+    'LIBPATH',
+    'Path',
+    'Platform',
+    /^VCTools/,
+    /^VSCMD_/,
+    /^WindowsSDK/i,
+]
 
 function getPlatform() {
     if (process.platform === 'win32') {
@@ -108,28 +120,66 @@ function getPlatform() {
     throw new Error(`Unsupported platform '${process.platform}'`)
 }
 
+async function setupMsvc() {
+    try {
+        core.info('setup msvc');
+
+        var installationPath = "";
+        const options = {};
+        options.listeners = {
+            stdout: (data) => {
+                var output = data.toString();
+                installationPath += output;
+            }
+        };
+        await exec.exec('"%ProgramFiles(x86)%\\Microsoft Visual Studio\\Installer\\vswhere.exe"', ['-latest', '-products', '*', '-requires', 'Microsoft.VisualStudio.Component.VC.Tools.x86.x64', '-property', 'installationPath'], options);
+        core.info(`installationPath = ${installationPath}`);
+
+        const vcVarsAll = path.join(installationPath, 'VC\\Auxiliary\\Build\\vcvarsall.bat')
+        const arch = core.getInput('arch');
+        const { stdout } = await exec.exec('cmd.exe', ['/q', '/c', '*', vcVarsAll, arch, '&', 'set']);
+        var environment = stdout.split('\r\n')
+        for (let string of environment) {
+            const [name, value] = string.split('=')
+            for (let pattern of InterestingVariables) {
+                if (name.match(pattern)) {
+                    core.exportVariable(name, value)
+                    break
+                }
+            }
+        }
+    }
+    catch (error) {
+        throw error
+    }
+}
+
+async function setupNinja(platform, luamakeDir) {
+    if (platform === 'msvc') {
+        await setupMsvc()
+        core.addPath(luamakeDir + "\\tools")
+    }
+    else if (platform === 'macos') {
+        await exec.exec('brew', ['install', 'ninja'], { encoding: 'utf8' })
+    }
+    else if (platform === 'linux') {
+        await exec.exec('sudo', ['apt-get', 'install', '-y', 'ninja-build'], { encoding: 'utf8' })
+    }
+}
+
 async function run() {
     try {
         await exec.exec('git', ['clone', '--recurse-submodules', '-j8', '--depth', '1', 'https://github.com/actboy168/luamake'], { encoding: 'utf8' })
-
         const platform = getPlatform()
         const luamakeDir = path.resolve(process.cwd(), 'luamake')
-        if (platform === 'msvc') {
-            core.addPath(luamakeDir + "\\tools")
-        }
-        else if (platform === 'macos') {
-            await exec.exec('brew', ['install', 'ninja'], { encoding: 'utf8' })
-        }
-        else if (platform === 'linux') {
-            await exec.exec('sudo', ['apt-get', 'install', '-y', 'ninja-build'], { encoding: 'utf8' })
-        }
-
+        await setupNinja(platform, luamakeDir)
         await exec.exec('ninja', ['-f', 'ninja/' + platform + '.ninja'], { encoding: 'utf8', cwd: luamakeDir })
         core.addPath(luamakeDir)
     } catch (error) {
         core.setFailed(error.message)
     }
 }
+
 run()
 
 
--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const core = require('@actions/core')
 const exec = require('@actions/exec')
 const process = require('process')
 const path = require('path')
+const execFile = require('util').promisify(require('child_process').execFile)
+
+const InterestingVariables = [
+    'INCLUDE',
+    'LIB',
+    'LIBPATH',
+    'Path',
+    'Platform',
+    /^VCTools/,
+    /^VSCMD_/,
+    /^WindowsSDK/i,
+]
 
 function getPlatform() {
     if (process.platform === 'win32') {
@@ -16,26 +28,64 @@ function getPlatform() {
     throw new Error(`Unsupported platform '${process.platform}'`)
 }
 
+async function setupMsvc() {
+    try {
+        core.info('setup msvc');
+
+        var installationPath = "";
+        const options = {};
+        options.listeners = {
+            stdout: (data) => {
+                var output = data.toString();
+                installationPath += output;
+            }
+        };
+        await exec.exec('"%ProgramFiles(x86)%\\Microsoft Visual Studio\\Installer\\vswhere.exe"', ['-latest', '-products', '*', '-requires', 'Microsoft.VisualStudio.Component.VC.Tools.x86.x64', '-property', 'installationPath'], options);
+        core.info(`installationPath = ${installationPath}`);
+
+        const vcVarsAll = path.join(installationPath, 'VC\\Auxiliary\\Build\\vcvarsall.bat')
+        const arch = core.getInput('arch');
+        const { stdout } = await exec.exec('cmd.exe', ['/q', '/c', '*', vcVarsAll, arch, '&', 'set']);
+        var environment = stdout.split('\r\n')
+        for (let string of environment) {
+            const [name, value] = string.split('=')
+            for (let pattern of InterestingVariables) {
+                if (name.match(pattern)) {
+                    core.exportVariable(name, value)
+                    break
+                }
+            }
+        }
+    }
+    catch (error) {
+        throw error
+    }
+}
+
+async function setupNinja(platform, luamakeDir) {
+    if (platform === 'msvc') {
+        await setupMsvc()
+        core.addPath(luamakeDir + "\\tools")
+    }
+    else if (platform === 'macos') {
+        await exec.exec('brew', ['install', 'ninja'], { encoding: 'utf8' })
+    }
+    else if (platform === 'linux') {
+        await exec.exec('sudo', ['apt-get', 'install', '-y', 'ninja-build'], { encoding: 'utf8' })
+    }
+}
+
 async function run() {
     try {
         await exec.exec('git', ['clone', '--recurse-submodules', '-j8', '--depth', '1', 'https://github.com/actboy168/luamake'], { encoding: 'utf8' })
-
         const platform = getPlatform()
         const luamakeDir = path.resolve(process.cwd(), 'luamake')
-        if (platform === 'msvc') {
-            core.addPath(luamakeDir + "\\tools")
-        }
-        else if (platform === 'macos') {
-            await exec.exec('brew', ['install', 'ninja'], { encoding: 'utf8' })
-        }
-        else if (platform === 'linux') {
-            await exec.exec('sudo', ['apt-get', 'install', '-y', 'ninja-build'], { encoding: 'utf8' })
-        }
-
+        await setupNinja(platform, luamakeDir)
         await exec.exec('ninja', ['-f', 'ninja/' + platform + '.ninja'], { encoding: 'utf8', cwd: luamakeDir })
         core.addPath(luamakeDir)
     } catch (error) {
         core.setFailed(error.message)
     }
 }
+
 run()
