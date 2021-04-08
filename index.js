cc77bb87c87d579238cf6a8db3b75f9c635e66c2
actboy168 <actboy168@gmail.com>
2021-04-08 13:54:23 +0800

    更新

--- index.js
+++ index.js
@@ -12,14 +12,16 @@ async function run() {
         }
         else if (process.platform === 'darwin') {
             await exec.exec('brew', ['install', 'ninja'])
-            await exec.exec('ninja', ['-f', 'ninja/macos.ninja'], { cwd: luamakeDir })
+            await exec.exec('chmod', ['+x', './compile/install.sh'], { cwd: luamakeDir })
+            await exec.exec('./compile/install.sh', { cwd: luamakeDir })
         }
         else if (process.platform === 'linux') {
             await exec.exec('sudo', ['apt-get', 'update'])
             await exec.exec('sudo', ['apt-get', 'install', '-y', 'libreadline-dev', 'ninja-build'])
             await exec.exec('sudo', ['update-alternatives', '--install', '/usr/bin/gcc', 'gcc', '/usr/bin/gcc-9', '100'])
             await exec.exec('sudo', ['update-alternatives', '--install', '/usr/bin/g++', 'g++', '/usr/bin/g++-9', '100'])
-            await exec.exec('ninja', ['-f', 'ninja/linux.ninja'], { cwd: luamakeDir })
+            await exec.exec('chmod', ['+x', './compile/install.sh'], { cwd: luamakeDir })
+            await exec.exec('./compile/install.sh', { cwd: luamakeDir })
         }
         else {
             throw new Error(`Unsupported platform '${process.platform}'`)
