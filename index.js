6393111138ce186ae2d67522429b1db1d3e906b0
actboy168 <actboy168@gmail.com>
2022-12-13 09:53:44 +0800

    跳过测试

--- index.js
+++ index.js
@@ -8,20 +8,20 @@ async function run() {
         await exec.exec('git', ['clone', '--recurse-submodules', '-j8', '--depth', '1', 'https://github.com/actboy168/luamake'], { encoding: 'utf8' })
         const luamakeDir = path.resolve('./luamake')
         if (process.platform === 'win32') {
-            await exec.exec('cmd.exe', ['/q', '/c', '.\\compile\\build.bat'], { cwd: luamakeDir })
+            await exec.exec('cmd.exe', ['/q', '/c', '.\\compile\\build.bat', 'build'], { cwd: luamakeDir })
         }
         else if (process.platform === 'darwin') {
             await exec.exec('brew', ['install', 'ninja'])
-            await exec.exec('chmod', ['+x', 'compile/install.sh'], { cwd: luamakeDir })
-            await exec.exec('compile/build.sh', [], { cwd: luamakeDir })
+            await exec.exec('chmod', ['+x', 'compile/build.sh'], { cwd: luamakeDir })
+            await exec.exec('compile/build.sh', ["build"], { cwd: luamakeDir })
         }
         else if (process.platform === 'linux') {
             await exec.exec('sudo', ['apt-get', 'update'])
             await exec.exec('sudo', ['apt-get', 'install', '-y', 'libreadline-dev', 'ninja-build'])
             await exec.exec('sudo', ['update-alternatives', '--install', '/usr/bin/gcc', 'gcc', '/usr/bin/gcc-9', '100'])
             await exec.exec('sudo', ['update-alternatives', '--install', '/usr/bin/g++', 'g++', '/usr/bin/g++-9', '100'])
-            await exec.exec('chmod', ['+x', 'compile/install.sh'], { cwd: luamakeDir })
-            await exec.exec('compile/build.sh', [], { cwd: luamakeDir })
+            await exec.exec('chmod', ['+x', 'compile/build.sh'], { cwd: luamakeDir })
+            await exec.exec('compile/build.sh', ["build"], { cwd: luamakeDir })
         }
         else {
             throw new Error(`Unsupported platform '${process.platform}'`)
