4d3fba7436ecfec449630df7dd7e8082a537fad2
actboy168 <actboy168@gmail.com>
2023-03-02 10:39:55 +0800

    换个目录

--- index.js
+++ index.js
@@ -7,47 +7,51 @@ const path = require('path')
 async function doAction(paths, key, action) {
     const restoreKey = await cache.restoreCache(paths, key, []);
     if (restoreKey !== undefined) {
+        console.debug(`Cache ${key} restore.`);
         return false
     }
     await action()
     try {
         await cache.saveCache(paths, key);
+        console.debug(`Cache ${key} saved.`);
     } catch (error) {
+        console.debug(error.message);
     }
     return true
 }
 
 async function run() {
     try {
-        await exec.exec('git', ['clone', '--recurse-submodules', '-j8', '--depth', '1', 'https://github.com/actboy168/luamake'], { encoding: 'utf8' })
-        const luamakeDir = path.resolve('./luamake')
-        const hash = (await exec.getExecOutput('git', ['rev-parse', 'HEAD'], { cwd: luamakeDir })).stdout.trim();
+        const cachedir = process.env['RUNNER_TOOL_CACHE']
+        await exec.exec('git', ['clone', '--recurse-submodules', '-j8', '--depth', '1', 'https://github.com/actboy168/luamake'], { cwd: cachedir, encoding: 'utf8' })
+        const luamakedir = path.join(cachedir, 'luamake')
+        const hash = (await exec.getExecOutput('git', ['rev-parse', 'HEAD'], { cwd: luamakedir })).stdout.trim();
         if (process.platform === 'win32') {
             const paths = [
-                './luamake/luamake.exe',
-                './luamake/tools/lua54.dll'
+                path.join(luamakedir, 'luamake.exe'),
+                path.join(luamakedir, 'tools', 'lua54.dll')
             ]
             const key = [ "windows", hash ].join("-")
             await doAction(paths, key, async function() {
-                await exec.exec('cmd.exe', ['/q', '/c', '.\\compile\\build.bat', 'notest'], { cwd: luamakeDir })
+                await exec.exec('cmd.exe', ['/q', '/c', '.\\compile\\build.bat', 'notest'], { cwd: luamakedir })
             })
         }
         else if (process.platform === 'darwin') {
             await exec.exec('brew', ['install', 'ninja'])
             const paths = [
-                './luamake/luamake'
+                path.join(luamakedir, 'luamake')
             ]
             const key = [ "macos", hash ].join("-")
             await doAction(paths, key, async function() {
-                await exec.exec('chmod', ['+x', 'compile/build.sh'], { cwd: luamakeDir })
-                await exec.exec('compile/build.sh', ["notest"], { cwd: luamakeDir })
+                await exec.exec('chmod', ['+x', 'compile/build.sh'], { cwd: luamakedir })
+                await exec.exec('compile/build.sh', ["notest"], { cwd: luamakedir })
             })
         }
         else if (process.platform === 'linux') {
             await exec.exec('sudo', ['update-alternatives', '--install', '/usr/bin/gcc', 'gcc', '/usr/bin/gcc-9', '100'])
             await exec.exec('sudo', ['update-alternatives', '--install', '/usr/bin/g++', 'g++', '/usr/bin/g++-9', '100'])
             const paths = [
-                './luamake/luamake'
+                path.join(luamakedir, 'luamake')
             ]
             const version = (await exec.getExecOutput('lsb_release', ['-r', '-s'])).stdout.trim();
             const key = [ "ubuntu", version, hash ].join("-")
@@ -56,8 +60,8 @@ async function run() {
                 await exec.exec('sudo', ['apt-get', 'install', '-y', 'libreadline-dev', 'ninja-build'])
                 await exec.exec('sudo', ['update-alternatives', '--install', '/usr/bin/gcc', 'gcc', '/usr/bin/gcc-9', '100'])
                 await exec.exec('sudo', ['update-alternatives', '--install', '/usr/bin/g++', 'g++', '/usr/bin/g++-9', '100'])
-                await exec.exec('chmod', ['+x', 'compile/build.sh'], { cwd: luamakeDir })
-                await exec.exec('compile/build.sh', ["notest"], { cwd: luamakeDir })
+                await exec.exec('chmod', ['+x', 'compile/build.sh'], { cwd: luamakedir })
+                await exec.exec('compile/build.sh', ["notest"], { cwd: luamakedir })
             })) {
                 await exec.exec('sudo', ['apt-get', 'update'])
                 await exec.exec('sudo', ['apt-get', 'install', '-y', 'ninja-build'])
@@ -68,8 +72,8 @@ async function run() {
         else {
             throw new Error(`Unsupported platform '${process.platform}'`)
         }
-        core.debug(`Added to PATH: ${luamakeDir}`)
-        core.addPath(luamakeDir)
+        core.debug(`Added to PATH: ${luamakedir}`)
+        core.addPath(luamakedir)
     } catch (error) {
         core.setFailed(error.message)
     }
